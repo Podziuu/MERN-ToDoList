1b89cdc2dfe5801be0a62ac4c82a3bcc84fcc3e7
Bartosz  Podemski <bartekpodemski@gmail.com>
2023-07-18 15:52:18 +0200

    Adding error handler

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -17,8 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use("/api/users", userRoutes)
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready"));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
