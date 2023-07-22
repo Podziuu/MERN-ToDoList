efa7a1dda184d74413c698ff44de529ebad90219
Bartosz  Podemski <bartekpodemski@gmail.com>
2023-07-22 14:54:14 +0200

    Adding user Api functions

--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import uiSlice from "./ui-slice";
+import uiSlice from "./slices/ui-slice";
+import authReducer from "./slices/authSlice";
+import { apiSlice } from "./slices/apiSlice";
 
 export const store = configureStore({
   reducer: {
     ui: uiSlice,
+    auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
