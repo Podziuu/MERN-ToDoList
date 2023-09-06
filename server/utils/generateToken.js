97d74d803415363489a060f0dcaaad9289f9718f
Bartosz  Podemski <bartekpodemski@gmail.com>
2023-09-06 20:59:05 +0200

    Fix v3

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -10,6 +10,7 @@ const generateToken = (res, userId) => {
     secure: process.env.NODE_ENV !== "development",
     sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
+    domain: "64f8c3bde22afe11d9d4937f--strong-kleicha-518bbc.netlify.app"
   });
 };
 
